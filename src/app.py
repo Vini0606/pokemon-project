f8b978f62642bf452d5b0b24891a67b11df7a628
Vinicius de Paula R Carvalho <vinicius.de.paula01998@gmail.com>
2025-10-26 09:46:44 -0300

    Altera app.py

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 from datetime import datetime, timedelta
+from pathlib import Path
 
 # --- Configuração da Página ---
 # Usar o layout 'wide' para aproveitar melhor o espaço da tela
@@ -11,9 +12,9 @@ st.set_page_config(layout="wide", page_title="Dashboard de Análise")
 @st.cache_data
 def carregar_dados():
     """Gera um DataFrame fictício para o dashboard."""
-    df_atributos = pd.read_json(r'data\\raw\\pokemons_atributos.json')
+    df_atributos = Path("data") / "raw" / "pokemons_atributos.json"
 
-    df_combats = pd.read_json(r'data\\raw\\combats.json')
+    df_combats = Path("data") / "raw" / "combats.json"
 
     df_combats_winners = df_combats['winner'].value_counts().reset_index().rename(
     columns={'winner': 'id', 'index': 'pokemon_id', 'count':'wins'}
